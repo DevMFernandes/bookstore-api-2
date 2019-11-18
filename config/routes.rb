c6ed6bf8139f5f56439e1e6d03d2724f026d04fd
Marcos A. Fernandes <contato@marcos-fernandes.com>
2019-11-18 11:04:19 -0300

    resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/create'
-  get 'books/destroy'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
